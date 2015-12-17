af29ee274f2487da8833a06eb1bc2aa826cd0c77
h-in-git <hamid.am@outlook.com>
2015-12-16 22:06:03 -0800

    first commit
    Initial Commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        $scope.snippet = '<span style="color: yellow !important;">hi there</span>';
+        $scope.boolValue = true;
+        $scope.mystyle = { color: 'yellow' };
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+        $scope.cbChecked = false;
+        $scope.sortorder = 'name';
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2016',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: 'app/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of direction!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scope for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: 2,
+                    level: 'Introductory',
+                    abstract: 'This session take a closer look at scopes',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'John Doe',
+                    duration: 4,
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything!',
+                    upVoteCount: 0
+                }
+            ]
+        }
+
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
+    }
+);
